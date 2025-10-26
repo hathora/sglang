e25dd15a8c1af60ce3dc2934f8ab833de2eabf7e
Sidddharth Dhulipalla <dsiddharth@gmail.com>
2025-10-26 17:27:09 -0400

    switch to sglang_router

--- kimi-k2/entrypoint_router.sh
+++ kimi-k2/entrypoint_router.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 set -e
 
-# Environment setup
+# setup cache directories
 export SGL_DG_CACHE_DIR=/cache/2
 export SGLANG_DG_CACHE_DIR=/cache/2
 export DG_JIT_CACHE_DIR=/cache/2
 export HF_HOME=/cache/2
-# export SGLANG_ALLOW_OVERWRITE_LONGER_CONTEXT_LEN=1
 
-# Configuration
-export MODEL_PATH="moonshotai/Kimi-K2-Instruct"
+#additional server arguments
 export MASTER_PORT=20000
-export PORT=${PORT:-25000}
+export PORT=${PORT:-30000}
 export API_KEY="${API_KEY:-${HATHORA_APP_SECRET:-}}"
-
-# Tunables (env override friendly)
-export CONTEXT_LENGTH=${CONTEXT_LENGTH:-131072}           # e.g. 131072 for K2 max
-export MEM_FRACTION_STATIC=${MEM_FRACTION_STATIC:-0.70} # e.g. 0.70–0.80 recommended
-export QUANTIZATION=${QUANTIZATION:-fp8}
 
 # Determine IB interfaces
 IB_IFACES=""
@@ -45,15 +38,23 @@ fi
 # Determine node role
 if [ -z "$HATHORA_INITIAL_ROOM_CONFIG" ]; then
   # Primary node
-  MASTER_IP=$HATHORA_PRIVATE_IP
-  NODE_RANK=0
+  export MASTER_IP=$HATHORA_PRIVATE_IP
+  export NODE_RANK=0
   python3 create_hathora_room.py "$MASTER_IP"
-  SERVER_ARGS="--host 0.0.0.0 --port $PORT --api-key $API_KEY"
 else
   # Secondary node
-  MASTER_IP=$(python3 -c "import json,os; print(json.loads(os.environ['HATHORA_INITIAL_ROOM_CONFIG'])['master_ip'])")
-  NODE_RANK=1
-  SERVER_ARGS=""
+  export MASTER_IP=$(python3 -c "import json,os; print(json.loads(os.environ['HATHORA_INITIAL_ROOM_CONFIG'])['master_ip'])")
+  export NODE_RANK=1
+fi
+
+# check PRESET and if the preset file exists
+if [ -z "$PRESET" ]; then
+  echo "PRESET is not set"
+  exit 1
+elif [ ! -f "presets/$PRESET.template" ]; then
+  echo "Preset file presets/$PRESET.template not found"
+  echo "Available presets: $(ls presets/*.template)"
+  exit 1
 fi
 
 # if DEBUG is set, then start a netcat listener on port
@@ -65,17 +66,9 @@ if [ -n "$DEBUG" ]; then
   done
 fi
 
-# Launch SGLang server
-exec python3 -m sglang.launch_server \
-  --model-path "$MODEL_PATH" \
-  --tp 16 \
-  --dist-init-addr "$MASTER_IP:$MASTER_PORT" \
-  --nnodes 2 \
-  --node-rank $NODE_RANK \
-  --context-length "$CONTEXT_LENGTH" \
-  --mem-fraction-static "$MEM_FRACTION_STATIC" \
-  --quantization "$QUANTIZATION" \
-  --trust-remote-code \
-  --tool-call-parser kimi_k2 \
-  $SERVER_ARGS \
-  2>&1
+# unwrap the arguments from the template file and pass them to the launch_server command
+export args=$(envsubst < presets/$PRESET.template | tr '\n' ' ')
+echo "sglang_router.launch_server arguments: $args"
+
+# launch the server
+exec python3 -m sglang_router.launch_server $args 2>&1
